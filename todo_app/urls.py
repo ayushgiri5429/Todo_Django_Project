ec90b34c32bb38fd758a310595d0aeb3b85e242d
Ayush Giri <pyayushgiri@gmail.com>
2025-05-20 15:42:35 +0545

    url updated

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from todo_app import views
 urlpatterns = [
     path("", views.todo_list),
-    path("delete/<int:id>/", views.todo_delete),
-    path("create/", views.todo_create),
-    path("update/<int:id>/", views.todo_update),
+    path("delete/<int:id>/", views.todo_delete, name="todo-list"),
+    path("create/", views.todo_create, name="todo-delete"),
+    path("update/<int:id>/", views.todo_update, name="todo-update"),
 ]
